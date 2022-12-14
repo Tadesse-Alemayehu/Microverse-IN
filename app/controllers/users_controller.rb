43d6b38f06eb24ed85ab4ec972994258f97edfe2
Tadesse-Alemayehu <tadalem26@gmail.com>
2022-12-14 04:18:42 +0300

    Update: render for json

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,13 @@ class UsersController < ApplicationController
   def index
     p "_________________________________runing users index_______________________________________________"
     UsersWorker.perform_async()
+    @users=User.all.to_a
+    # p "User are #{JSON.parse(@users[0].to_json)}"
+        respond_to do |format|
+          format.html # index.html.erb
+          format.json {render json: @users}
+        end
+
   end
 
   def show
--- app/workers/users_worker.rb
+++ app/workers/users_worker.rb
@@ -6,6 +6,8 @@ class UsersWorker
     response = RestClient.get(url, headers={Authorization: ENV['USERS_API_KEY']})
     if response.code==200
           user_data = JSON.parse(response.body)
+           ## because we use the id from the API there is les chance that we will have Repeated users.
+           ## I use the ID from the API server on purpose
           User.create!(user_data)
     end
   end
