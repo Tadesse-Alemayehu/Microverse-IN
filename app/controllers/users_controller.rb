ea26c36bfa3cd56b509d52f7bcc9bc15df6e8e33
Tadesse-Alemayehu <tadalem26@gmail.com>
2022-12-14 10:09:15 +0300

    Update: json render for use show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,10 +4,10 @@ class UsersController < ApplicationController
     if params[:status] && params[:status] !="All"
 
     @users=User.where(status: params[:status]).paginate(:page => params[:page], :per_page => params[:limit] || 2)
-    p @users.length
+
     else
     @users=User.paginate(:page => params[:page], :per_page => params[:limit]||2)
-p @users.length
+
     end
 
         respond_to do |format|
@@ -18,5 +18,9 @@ p @users.length
 
   def show
     @user=User.find params[:id]
+        respond_to do |format|
+          format.html # index.html.erb
+          format.json {render json: @user}
+        end
   end
 end
