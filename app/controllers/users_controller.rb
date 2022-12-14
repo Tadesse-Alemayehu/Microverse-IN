fb961c640662f3f29e13142b022875db37b033c6
Tadesse-Alemayehu <tadalem26@gmail.com>
2022-12-14 06:21:48 +0300

    Add: offset and limit for the users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,13 +2,16 @@ class UsersController < ApplicationController
   def index
     p "_________________________________runing users index_______________________________________________"
     UsersWorker.perform_async()
-    @users=User.all.to_a
-    # p "User are #{JSON.parse(@users[0].to_json)}"
+    @users=User.all
+    # paginate on what has already been loaded (remove hitting the database more frequently)
+    offset=params[:offset]&.to_i || 0
+    limit=params[:limit]&.to_i || 2
+    p "limit is #{offset} offset is #{offset}"
+    @current_user=@users.slice(offset,limit)
         respond_to do |format|
           format.html # index.html.erb
           format.json {render json: @users}
         end
-
   end
 
   def show
